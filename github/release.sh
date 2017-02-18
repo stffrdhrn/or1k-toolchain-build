da12f23ebd3433cf6371d1e81277dcd65ca191da
Stafford Horne <shorne@gmail.com>
2017-02-19 08:10:56 +0900

    Fix various issues in release
    - The base dir for release scripts was not setup correct after move
      to github dir
    - The dockerfile env var location was not good because updates there
      would cause whole rebuild of everything below it
    - The release scripts now verify tags and createpatches

--- github/release.sh
+++ github/release.sh
@@ -3,27 +3,60 @@
 set -ex
 
 GCC_VERSION=5.4.0
-RELEASE=`date -u +%Y%m%d`
+
+# The github originization we want to create the or1k-gcc release at
+GITHUB_ORG=openrisc
+# The remote we want to upload tags to on the remote repo
+GIT_REMOTE=or1k
+# The location of your git repo
 GIT_HOME=$HOME/work/openrisc/or1k-gcc
 
+# Dry run examples
+#GITHUB_ORG=stffrdhrn
+#GIT_REMOTE=shorne
+
 DIR=`dirname $0`
+pushd $DIR ; DIR=$PWD ; popd
 . ${DIR}/github.api
 
-tag="or1k-${GCC_VERSION}-${RELEASE}"
-branch="or1k-5.4.0"
+
+release=`date -u +%Y%m%d`
+base_tag="gcc-${GCC_VERSION//./_}-release"
+
+branch="or1k-${GCC_VERSION}"
+tag="or1k-${GCC_VERSION}-${release}"
+musl_branch="musl-${GCC_VERSION}"
+musl_tag="or1k-musl-${GCC_VERSION}-${release}"
 
 # Create and push git tag
-pushd $GIT_HOME
-  cur_branch=`git status -sb -uno`
-  if [[ $cur_branch != "## $branch" ]] ; then
-     echo "Failed when creating git tag, current branch status is $cur_branch, not $branch"
-     exit 1
-  fi
-  git tag -sf $tag
-  git push -f shorne $tag
-popd
+git_tagnpush() {
+  declare branch=$1 ; shift
+  declare tag=$1 ; shift
+  declare remote=$1 ; shift
 
-github_release "stffrdhrn/or1k-gcc" \
+  # check if the tag already exists at the destination, and return
+  git ls-remote --tags --refs $remote | grep -e "/$tag$" && return 0
+
+  pushd $GIT_HOME
+    git checkout $branch
+    declare cur_branch=`git status -sb -uno`
+    if [[ $cur_branch != "## $branch" ]] ; then
+      echo "Failed when creating git tag, current branch status is $cur_branch, not $branch"
+      exit 1
+    fi
+    git diff ${base_tag}..${branch} > $DIR/gcc-${tag}.patch
+
+    # If we got this far the tag does exist at the remote so create and push it
+    git tag -sf $tag
+    git push -f $remote $tag
+  popd
+}
+
+git_tagnpush ${musl_branch} ${musl_tag} ${GIT_REMOTE}
+git_tagnpush ${branch} ${tag} ${GIT_REMOTE}
+
+# Create release
+github_release "${GITHUB_ORG}/or1k-gcc" \
   "${tag}" \
   "${branch}" \
   "OpenRISC ${GCC_VERSION} snapshot release source and binaries"
